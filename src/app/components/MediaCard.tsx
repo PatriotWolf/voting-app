09bc347e2323eee3d74fbee0701cb41f8c4e39e7
Mr.Worldwide <syahfis@gmail.com>
2024-01-18 14:20:17 +0800

    feature: Mui install && base theme && remove all css from next init

---
+++ src/app/components/MediaCard.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import Image from 'next/image';
+
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { Category } from 'app/types';
+
+const MediaCard = ({ title, description }: Category) => {
+  return (
+    <Card>
+      <Image
+        alt="Random image"
+        src="https://source.unsplash.com/random"
+        width={640}
+        height={480}
+        style={{
+          maxWidth: '100%',
+          height: '200px',
+          objectFit: 'cover',
+        }}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" variant="contained">
+          Go
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default MediaCard;
---
+++ src/app/constants.ts
@@ -0,0 +1,6 @@
+import { Category } from './types';
+
+export const catergories: Category[] = [
+  { title: 'Category 1', description: 'This is category detail for 1.' },
+  { title: 'Category 2', description: 'This is category detail for 2.' },
+];
